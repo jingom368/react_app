686c778d377b93af2d42cf6fc11e18abca67a0d2
jignom368 <jingom369@gmail.com>
2022-07-07 09:42:32 +0900

    update

--- mynotes/urls.py
+++ mynotes/urls.py
@@ -1,4 +1,4 @@
-"""test_django URL Configuration
+"""mynotes URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api/', include(('api.urls', 'api'), namespace='api')),
-    path('', TemplateView.as_view(template_name='index.html'), name='test'),
+    path('api/', include('api.urls')),
+    path('', TemplateView.as_view(template_name='index.html'), name='note-list'),
+    path('note/<str:pk>/', TemplateView.as_view(template_name='index.html'), name='note'),
+    path('note/new/', TemplateView.as_view(template_name='index.html'), name='note-write'),
 ]
